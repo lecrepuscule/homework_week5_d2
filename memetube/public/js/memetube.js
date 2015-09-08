4166ebe4f18d6aa4fc76cafe2d8f7569bcb626d7
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-08 20:40:09 +0100

    show video client side is working

--- memetube/public/js/memetube.js
+++ memetube/public/js/memetube.js
@@ -15,8 +15,14 @@ function showVideo(){
   var action = "/videos/" + $(this).data("id");
   request(action, "get").done(function(response){
     console.log(response);
-    playVideo();
+    screenVideo(response);
   })
+}
+
+function screenVideo(video){
+  var videoScreen = $("#video-screen");
+  videoScreen.empty();
+  $("<iframe class='player' type='text/html' width='640' height='390' src='http://www.youtube.com/embed/" + video.url + "' frameborder='0'></iframe>").appendTo(videoScreen);
 }
 
 function request(action, method, data){
